e3686d310af6a018a60f4c8754e0607e82fa0806
Alex Rivera <alex.rivera@itech.media>
2021-08-25 12:05:37 +0100

    moved upload component to home page to replicate what wetransfer have

--- resources/js/components/Photos.vue
+++ resources/js/components/Photos.vue
@@ -2,14 +2,10 @@
     <div class="p-6">
         <h1 class="text-3xl mb-4">Uploaded Photos</h1>
 
+        <PhotoUpload />
+
         <div class="grid-cols-3 gap-4 hidden md:grid">
             <div v-for="partition in partitionedPhotos3">
-                <img :src="photo.url" class="w-full mt-4 object-cover rounded-lg" v-for="photo in partition"/>
-            </div>
-        </div>
-
-        <div class="grid-cols-2 gap-4 grid md:hidden">
-            <div v-for="partition in partitionedPhotos2">
                 <img :src="photo.url" class="w-full mt-4 object-cover rounded-lg" v-for="photo in partition"/>
             </div>
         </div>
@@ -17,20 +13,22 @@
 </template>
 
 <script>
+import PhotoUpload from './PhotoUpload.vue';
+
 export default {
     name: 'Photos',
-
+    components: {
+        PhotoUpload
+    },
     data() {
         return {
             photos: [],
         };
     },
-
     mounted() {
         this.loadPhotos();
         this.setupWebsockets();
     },
-
     computed: {
         partitionedPhotos3() {
             return this.partition(this.photos, 3);
@@ -40,7 +38,6 @@ export default {
             return this.partition(this.photos, 2);
         },
     },
-
     methods: {
         partition(photos, size) {
             let index = 0;
@@ -86,3 +83,9 @@ export default {
     },
 };
 </script>
+
+<style scoped>
+.text-3xl {
+    color: pink;
+}
+</style>
